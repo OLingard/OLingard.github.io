951951e1ae8c62531b9900a99731026b97eb0d21
OLingard <oscarlingard@gmail.com>
2017-07-18 11:03:20 +0200

    Draft 1.2.5

--- general.js
+++ general.js
@@ -26,7 +26,7 @@ var items = document.querySelectorAll(".timeline li");
 function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
   return (
-    rect.top >= 0 &&
+    rect.top + 120 >= 0 &&
     rect.left >= 0 &&
     rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
     rect.right <= (window.innerWidth || document.documentElement.clientWidth)
