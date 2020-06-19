c15a3589afbb848d2c3ffc2b7b51056752485b58
Oscar Lingard <OscarLingard@rentalcars.com>
2020-06-19 09:52:55 +0100

    19/6/20 Improval of naming conventions Removal of Package Lock Analyst Update

--- general.js
+++ general.js
@@ -1,27 +1,19 @@
 function switchClass(x) {
-        if(document.getElementsByClassName("item active").length){
-            document.getElementsByClassName("item active")[0].className = "item";
+        if(x.parentElement.getElementsByClassName("item active").length){
+            x.parentElement.getElementsByClassName("item active")[0].className = "item";
         }
         x.className = "item active";
         return true;
-    }
-function switchClass2(x) {
-        if(document.getElementsByClassName("item active").length){
-            document.getElementsByClassName("item active")[1].className = "item";
-        }
-        x.className = "item active";
-        return true;
-    }
+}
 
-function ass1() {
+function linkedinAnchorLink() {
   $(location).attr('href', 'http://linkedin.com/in/oscarlingard');
 }
 
-function ass2() {
+function emailAnchorLink() {
   $(location).attr('href', 'mailto:oscarlingard@gmail.com');
 }
 
-var items = document.querySelectorAll(".timeline li");
 
 function isElementInViewport(el) {
   var rect = el.getBoundingClientRect();
@@ -34,9 +26,10 @@ function isElementInViewport(el) {
 }
 
 function callbackFunc() {
-  for (i = 0; i < items.length; i++) {
-    if (isElementInViewport(items[i])) {
-      items[i].classList.add("in-view");
+  var timelineItems = document.querySelectorAll(".timeline li");
+  for (i = 0; i < timelineItems.length; i++) {
+    if (isElementInViewport(timelineItems[i])) {
+      timelineItems[i].classList.add("in-view");
     }
   }
 }
