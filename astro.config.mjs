1f277bbfd43ebf89d1104143f5c64d118adbfb47
Oscar Lingard <oscar.lingard@thehutgroup.com>
2024-08-04 21:00:19 +0100

    Changing to yarn and updating for thg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), image()]
+  integrations: [react()]
 });
